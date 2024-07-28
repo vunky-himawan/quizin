bcb4e1f579565a1e6012b4172bab8543fec2e80e
vunky-himawan <tingkerbeii44@gmail.com>
2024-07-29 05:12:43 +0700

    chore: fix db connection and fix handle answer

--- src/api/database/index.ts
+++ src/api/database/index.ts
@@ -4,10 +4,20 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-if (!process.env.DB_URL) {
+if (
+  !process.env.DB_HOST &&
+  !process.env.DB_USERNAME &&
+  !process.env.DB_PASSWORD &&
+  !process.env.DB_DATABASE
+) {
   throw new Error("DB credentials error");
 }
 
-const connection = mysql.createConnection(process.env.DB_URL);
+const connection = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
 
 export const db = drizzle(connection);
